290f3f054b6e69691592b1781ec3b0510c7fb1b2
p.chechurova <chechurovapolina@mail.ru>
2025-03-21 01:21:48 +0000

    комментарии к реализации

--- src/solution.cc
+++ src/solution.cc
@@ -9,14 +9,23 @@
 
 #include <cmath>
 
+/// \brief Необходимость учёта NaN значений при сравнении точек
+/// \remark Можно вынести задание в CMake через target_compile_definitions
 #define SOLUTION_NAN_AWARE_COMPARISONS 1
 
+/// \brief Точка на плоскости
 struct Point
 {
-  double x;
-  double y;
+  double x;  ///< Положение точки по оси X
+  double y;  ///< Положение точки по оси Y
 };
 
+/// \brief Сравнивает на равенство два double значения
+/// \details
+/// При SOLUTION_NAN_AWARE_COMPARISONS считает NaN == NaN и -NaN==-NaN.
+/// \param lhs Сравниваемое значение
+/// \param rhs Сравниваемое значение
+/// \returns Признак равенства lhs и rhs
 [[nodiscard]] static auto cmp_double_equal(double lhs, double rhs) -> bool
 {
 #if SOLUTION_NAN_AWARE_COMPARISONS
@@ -29,6 +38,13 @@ struct Point
 #endif
 }
 
+/// \brief Возвращает признак того, что первое double значение (lhs) меньше второго (rhs)
+/// \details
+/// При SOLUTION_NAN_AWARE_COMPARISONS считает NaN значения меньше не NaN значений,
+/// а при сравнении двух NaN значений lhs считается меньше rhs, если lhs равен -NaN, а rhs +NaN.
+/// \param lhs Первое сравниваемое значение (стоит слева от оператора \<)
+/// \param rhs Второе сравниваемое значение (стоит справа от оператора \<)
+/// \returns Если lhs меньше rhs, true, в остальных случаях - false
 [[nodiscard]] static auto cmp_double_less(double lhs, double rhs) -> bool
 {
 #if SOLUTION_NAN_AWARE_COMPARISONS
@@ -44,19 +60,41 @@ struct Point
 #endif
 }
 
+/// \brief Сравнивает две точки на равенство
+/// \details
+/// Две точки считаются равными, если положение первой точки на оси X равно положению второй точки
+/// на оси X, как и положение первой точки на оси Y равно положению второй точки на оси Y
+/// \param lhs Сравниваемая точка
+/// \param rhs Сравниваемая точка
+/// \returns Признак равенства точек
 [[nodiscard]] static auto cmp_points_equal(Point const& lhs, Point const& rhs) -> bool
 {
   return cmp_double_equal(lhs.x, rhs.x) and cmp_double_equal(lhs.y, rhs.y);
 }
 
+/// \brief Возвращает признак того, что первая точка (lhs) "меньше" второй точки (rhs)
+/// \details
+/// Точка lhs считается "меньше" точки rhs, если положение lhs на оси X меньше положения rhs на оси
+/// X либо если при одинаковых положениях lhs и rhs на оси X положение lhs на оси Y меньше положения
+/// rhs на оси Y.
+/// Используется для сортировки точек в std::set и получения отсортированного множества уникальных
+/// точек.
+/// \param lhs Первая сравниваемая точка (стоит слева от оператора \<)
+/// \param rhs Вторая сравниваемая точка (стоит справа от оператора \<)
+/// \returns Если lhs "меньше" rhs, true, в остальных случаях - false
+/// \see read_points_from_file
 [[nodiscard]] static auto cmp_points_less(Point const& lhs, Point const& rhs) -> bool
 {
   return cmp_double_equal(lhs.x, rhs.x) ? cmp_double_less(lhs.y, rhs.y)
                                         : cmp_double_less(lhs.x, rhs.x);
 }
 
+/// \brief Тип множества уникальных отсортированных точек
 using PointsSet = std::set<Point, std::decay_t<decltype(cmp_points_less)>>;
 
+/// \brief Считывает из файла точки и возвращает уникальные в виде отсортированного множества
+/// \param path Путь до файла, из которого производится чтение
+/// \returns Отсортированное множество уникальных считанных точек
 [[nodiscard]] static auto read_points_from_file(std::filesystem::path const& path) -> PointsSet
 {
   PointsSet result {cmp_points_less};
@@ -68,6 +106,15 @@ using PointsSet = std::set<Point, std::decay_t<decltype(cmp_points_less)>>;
   return result;
 }
 
+/// \brief Определяет отношение двух отсортированных множеств уникальных точек на плоскости
+/// \details
+/// Возвращает ISolution::Result::Equal, если A содержит те же точки, что и B.
+/// Возвращает ISolution::Result::BSubsetOfA, если B является подмножеством A.
+/// Возвращает ISolution::Result::ASubsetOfB, если A является подмножеством B.
+/// В остальных случаях возвращает ISolution::Result::NotEqual.
+/// \param a Множество А
+/// \param b Множество B
+/// \returns Отношение множеств A и B
 [[nodiscard]] static auto cmp_point_sets(PointsSet const& a, PointsSet const& b)
   -> ISolution::Result
 {
